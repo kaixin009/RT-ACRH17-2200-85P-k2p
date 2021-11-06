d22df5579412bf1cfc80e183345e9922d8cfa0d1
AK201314 <88940210+AK201314@users.noreply.github.com>
2021-11-06 16:56:13 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -21,4 +21,4 @@
 
 #echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
 
-echo 'src-git clone https://github.com/wiwizcom/WiFiPortal.git' >>feeds.conf.default
+#echo 'src-git clone https://github.com/wiwizcom/WiFiPortal.git' >>feeds.conf.default
